98962cb7873442555e15a9aa64982b2cec147481
kimdanhee <sgoiboy@gmail.com>
2022-07-16 16:34:39 +0900

    미사용 색상 제거 X, 색상 추가

--- app/src/main/java/damin/tothemoon/booklink/ui/theme/Color.kt
+++ app/src/main/java/damin/tothemoon/booklink/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package damin.tothemoon.booklink.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Gray100 = Color(0xFF333333)
+val Gray200 = Color(0xFF4F4F4F)
+val Gray300 = Color(0xFF828282)
+val Gray400 = Color(0xFFBDBDBD)
+val Gray500 = Color(0xFFE0E0E0)
+val Gray600 = Color(0xFFF2F2F2)
+
+val Yellow100 = Color(0xFFF2C94C)
+
+val Blue100 = Color(0xFF48A5FB)
+val Blue200 = Color(0xFF87C5FE)
