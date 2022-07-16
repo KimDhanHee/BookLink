05d7c85b8d70908d5388afbfbb5984c512edfa64
Kim Dany <sgoiboy@gmail.com>
2022-07-16 16:52:44 +0900

    theme (color, typo) 적용 (#1)
    * 미사용 색상 제거 X, 색상 추가
    
    * theme 색상 정리
    
    * 미사용 제거
    
    * statusBarColor
    
    * typo

--- app/src/main/java/damin/tothemoon/booklink/ui/theme/Type.kt
+++ app/src/main/java/damin/tothemoon/booklink/ui/theme/Type.kt
@@ -8,21 +8,39 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+  h1 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.W700,
+    fontSize = 24.sp
+  ),
+  h2 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.W700,
+    fontSize = 20.sp
+  ),
+  h3 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.W700,
+    fontSize = 16.sp
+  ),
+  h4 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.W700,
+    fontSize = 12.sp
+  ),
+  button = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.W700,
+    fontSize = 14.sp
+  ),
   body1 = TextStyle(
     fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
+    fontWeight = FontWeight.W500,
     fontSize = 16.sp
-  )
-  /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+  ),
+  body2 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.W500,
+    fontSize = 12.sp
+  ),
 )
