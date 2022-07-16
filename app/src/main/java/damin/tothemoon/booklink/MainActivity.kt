47f1b22b735313b053e445dff2ef218638a1ac57
kimdanhee <sgoiboy@gmail.com>
2022-07-16 22:40:52 +0900

    splash 화면 추가

--- app/src/main/java/damin/tothemoon/booklink/MainActivity.kt
+++ app/src/main/java/damin/tothemoon/booklink/MainActivity.kt
@@ -6,35 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import damin.tothemoon.booklink.ui.theme.BookLinkTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
+
     setContent {
       BookLinkTheme {
-        // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  BookLinkTheme {
-    Greeting("Android")
   }
 }
