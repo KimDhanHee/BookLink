9e803e739284c501b2951e89eff57186565ea3e0
kimdanhee <sgoiboy@gmail.com>
2022-08-14 10:13:10 +0900

    splash 1초 노출

--- app/src/main/java/damin/tothemoon/booklink/MainActivity.kt
+++ app/src/main/java/damin/tothemoon/booklink/MainActivity.kt
@@ -1,6 +1,7 @@
 package damin.tothemoon.booklink
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,11 +10,18 @@ import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import damin.tothemoon.booklink.ui.theme.BookLinkTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     installSplashScreen()
     super.onCreate(savedInstanceState)
+
+    findViewById<View>(android.R.id.content).viewTreeObserver.addOnPreDrawListener {
+      runBlocking { delay(1000) }
+      true
+    }
 
     setContent {
       BookLinkTheme {
